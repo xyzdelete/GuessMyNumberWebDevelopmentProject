6465e0aaabfaf2475b148cb31f971577c67b2350
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-13 01:41:10 +0200

    Handling Click Events

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
 
-document.querySelector(".message").textContent = "Correct Number!";
-
-console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
+    if (!guess) {
+        document.querySelector(".message").textContent = "⛔ No number!";
+    }
+});
