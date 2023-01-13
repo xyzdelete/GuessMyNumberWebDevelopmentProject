748e18065469e202ad42482655a8913445a8f4ea
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-13 13:03:07 +0200

    Implementing Highscores

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
+let highscore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess, typeof guess);
@@ -24,6 +26,11 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector(".number").style.width = "30rem";
 
         document.querySelector(".number").textContent = secretNumber;
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector(".highscore").textContent = highscore;
+        }
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "⚠️ Too high!";
