6a7a0085b2193d6142bb6415de37d59247275684
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-13 12:49:59 +0200

    Coding Challenge #1

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
     const guess = Number(document.querySelector(".guess").value);
@@ -24,6 +22,8 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector("body").style.backgroundColor = "#60b347";
 
         document.querySelector(".number").style.width = "30rem";
+
+        document.querySelector(".number").textContent = secretNumber;
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "⚠️ Too high!";
@@ -48,4 +48,15 @@ document.querySelector(".check").addEventListener("click", function () {
             document.querySelector(".score").textContent = score;
         }
     }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".guess").value = "";
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".message").textContent = "Start guessing...";
 });
